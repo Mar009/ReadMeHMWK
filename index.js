7627bc8300c634872e1690d66c17816c50c06e4b
MMMarz009 <mar0099887@gmail.com>
2020-07-08 18:54:12 -0500

    Added questions to index

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const fs=require("fs")
 
 // array of questions for user
 const questions = [
+    { type: "input", message: "What's the project title?", name: "title"},
+    
+    {type: "input", message: "What's the description?", name: "description"},
+    
+    {type: "input", message: "What are the table of contents?", name: "toc"},
+    
+    {type: "input", message: "What are the install instructions?", name: "install"},
+    
+    {type: "input", message: "What's the usage information?", name: "usage"},
+    
+    {type: "input", message: "Who contributed?", name: "contribution"},
+    
+    {type: "input", message: "What are thet tests?", name: "tests"},
+    
+    {type: "input", message: "What are the licenses?", name: "license"},
+    
+    {type: "input", message: "Where can people contact you for questions?", name: "contact"}
 
 ];
 
